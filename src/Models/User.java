d88a7cc045a606dae6542c07bc1187d421ea6ab3
iribuda <ibudaychieva@gmail.com>
2022-09-08 12:09:02 +0600

    User class edited

--- src/Models/User.java
+++ src/Models/User.java
@@ -1,5 +1,56 @@
 package Models;
 
 public class User {
+    private int id;
+    private String fName;
+    private String lName;
+    private String email;
+    private String password;
 
+    public User(String fName, String lName, String email, String password) {
+        this.fName = fName;
+        this.lName = lName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
